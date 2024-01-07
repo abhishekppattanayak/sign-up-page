0eefb81ccc7f3ca80850cabfe3319915185d380c
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2024-01-08 00:44:59 +0530

    Add html, css, js structure

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ let switchTitle = () => {
 }
 document.querySelector('footer > span').textContent = `Copyright © ${new Date().getFullYear()} `;
 
+Array.from(document.querySelectorAll('form > div > input')).forEach(element=> {
+    element.addEventListener('input', ()=>{
+        if(element.value == ''){
+            element.nextElementSibling.style.display = 'none';
+        }else {
+            element.nextElementSibling.style.display = 'inherit';
+        }
+    })
+})
+
+Array.from(document.querySelectorAll('button[type="reset"]')).forEach(element=>{
+    element.addEventListener('click', ()=>{
+        Array.from(document.querySelectorAll('form > div > input')).forEach(element=> element.nextElementSibling.style.display = 'none')
+    })
+});
+
+document.querySelector('input[placeholder="Confirm Password"').addEventListener('input', ()=>{
+    document.querySelector('input[placeholder="Confirm Password"').setAttribute('pattern', `${document.querySelector('#signup-password').value}`)
+});
+document.querySelector('#signup-password').addEventListener('input', ()=>{
+    document.querySelector('input[placeholder="Confirm Password"').setAttribute('pattern', `${document.querySelector('#signup-password').value}`)
+});
