f7dc3e630da930cdfb606ef6d3382ff860059d2d
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2024-01-09 00:42:53 +0530

    Add image sidebar, remove login form

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let switchTitle = () => {
-    document.querySelector('title').textContent = document.querySelector('title').textContent=='Sign-up'?'Login':'Sign-up';
-}
 document.querySelector('footer > span').textContent = `Copyright © ${new Date().getFullYear()} `;
+
+window.addEventListener('beforeunload', ()=> {
+    document.querySelectorAll('input').forEach(input => input.value = '')
+})
 
 Array.from(document.querySelectorAll('form > div > input')).forEach(element=> {
     element.addEventListener('input', ()=>{
@@ -13,15 +14,28 @@ Array.from(document.querySelectorAll('form > div > input')).forEach(element=> {
     })
 })
 
-Array.from(document.querySelectorAll('button[type="reset"]')).forEach(element=>{
-    element.addEventListener('click', ()=>{
-        Array.from(document.querySelectorAll('form > div > input')).forEach(element=> element.nextElementSibling.style.display = 'none')
-    })
-});
-
 document.querySelector('input[placeholder="Confirm Password"').addEventListener('input', ()=>{
     document.querySelector('input[placeholder="Confirm Password"').setAttribute('pattern', `${document.querySelector('#signup-password').value}`)
 });
-document.querySelector('#signup-password').addEventListener('input', ()=>{
+
+document.querySelector('input[placeholder="Password"').addEventListener('input', ()=>{
     document.querySelector('input[placeholder="Confirm Password"').setAttribute('pattern', `${document.querySelector('#signup-password').value}`)
+});
+document.querySelector('input[type="tel"]').addEventListener('keydown', function (e) {
+    // Allow: backspace, delete, tab, escape, enter, and numbers
+    if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
+        // Allow: Ctrl+A/Ctrl+C/Ctrl+V
+        (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+        (e.keyCode === 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+        (e.keyCode === 86 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: home, end, left, right, down, up
+        (e.keyCode >= 35 && e.keyCode <= 40)) {
+        // Allow the key
+        return;
+    }
+
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
 });
